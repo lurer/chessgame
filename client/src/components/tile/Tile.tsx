1b9c74b31d8418c2758cbc7d2fae6e55300328e2
Espen Zaal <espenbz@gmail.com>
2019-04-08 21:24:04 +0200

    Optional parameter to show Tile labels

--- client/src/components/tile/Tile.tsx
+++ client/src/components/tile/Tile.tsx
@@ -1,41 +1,47 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import styles from './tile.module.scss';
 
-export interface TileProps{
+export interface TileProps {
     color: string;
     row: number;
     file: string;
+    showLabels?: boolean;
 }
 
 const Tile = (props: TileProps) => {
 
-    const fileLabel = () : JSX.Element => {
-        if(props.row == 1)
+    const fileLabel = (): JSX.Element => {
+        if (props.row == 1)
             return <div className={[styles.label, styles.fileLabelA].join(" ")}>{props.file}</div>
         else if (props.row == 8)
             return <div className={[styles.label, styles.fileLabelH].join(" ")}>{props.file}</div>
         return <span></span>
     }
 
-    const rowLabel = () : JSX.Element => {
-        if(props.file == "A")
+    const rowLabel = (): JSX.Element => {
+        if (props.file == "A")
             return <div className={[styles.label, styles.rowLabel1].join(" ")}>{props.row}</div>
-        if(props.file == "H")
-            return <div className={[styles.label, styles.rowLabel8].join(" ")}>{props.row}</div>            
+        if (props.file == "H")
+            return <div className={[styles.label, styles.rowLabel8].join(" ")}>{props.row}</div>
         return <span></span>;
     }
 
-    return(
-        <div className={styles.tile} 
-            style={{background: props.color}} 
-            data-row={props.row} 
-            data-file={props.file} 
-            data-tile={props.file +""+props.row}>
-                {rowLabel()}
-                {fileLabel()}
-            
-            </div>
+    return (
+        <div className={styles.tile}
+            style={{ background: props.color }}
+            data-row={props.row}
+            data-file={props.file}
+            data-tile={props.file + "" + props.row}>
+            {props.showLabels &&
+                <Fragment>
+                    {rowLabel()}
+                    {fileLabel()}
+                </Fragment>
+            }
+
+
+        </div>
     )
 }
 
